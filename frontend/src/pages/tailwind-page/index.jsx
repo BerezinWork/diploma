3f9bf8f72d01ec59af1956a5bafb4123826f57a7
Illia Berezin <berezin.main@gmail.com>
2025-06-16 14:45:50 +0300

    Add Resume

--- frontend/src/pages/tailwind-page/index.jsx
+++
@@ -1,9 +0,0 @@
-const TailWind = () => {
-    return (
-        <div className="text-2xl font-bold p-10 bg-brand">
-            Hello from Tailwind!
-        </div>
-    )
-}
-
-export default TailWind;
